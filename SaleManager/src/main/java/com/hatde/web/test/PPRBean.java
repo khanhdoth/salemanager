fd083765a25cb4facd2efad6c732b66ed80d8fb7
Do <Do@192.168.1.4>
2013-09-25 21:00:49 +0200

    first

--- SaleManager/src/main/java/com/hatde/web/test/PPRBean.java
+++ SaleManager/src/main/java/com/hatde/web/test/PPRBean.java
@@ -3,7 +3,6 @@ package com.hatde.web.test;
 import java.io.Serializable;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
-import org.primefaces.context.RequestContext;
 
 @Named(value = "pprBean")
 @SessionScoped
@@ -18,11 +17,4 @@ public class PPRBean implements Serializable {
     public void setFirstname(String firstname) {
         this.firstname = firstname;
     }
-
-    public void viewCars() {
-        System.out.println("***** viewCard *****");
-        RequestContext requestContext = RequestContext.getCurrentInstance();
-        requestContext.addCallbackParam("isValid", true);
-    }
-
 }
