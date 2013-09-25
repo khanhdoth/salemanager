0cf040cd0313dcf293a3fec2600608c73aa297ca
Do <Do@192.168.1.4>
2013-09-26 00:02:17 +0200

    first

---
+++ SaleManager/src/main/java/com/hatde/salemanager/web/ContactController.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.hatde.salemanager.web;
+
+import com.hatde.salemanager.entities.Contact;
+import com.hatde.salemanager.services.ContactFacadeREST;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+/**
+ *
+ * @author Do
+ */
+//@Named(value = "contactController")
+//@SessionScoped
+@Named(value = "contactController")
+@SessionScoped
+public class ContactController implements Serializable {
+
+    @EJB
+    private ContactFacadeREST bean;
+
+    private List<Contact> list;
+
+    @PostConstruct
+    public void init() {
+        list = new ArrayList();
+        list = bean.findAll();
+    }
+
+    public ContactController() {
+    }
+
+    public List<Contact> getList() {
+        return list;
+    }
+
+    public void setList(List<Contact> list) {
+        this.list = list;
+    }
+}
