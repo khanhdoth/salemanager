c6d3de18dfae236649149261303708b24aa524e4
Do <Do@192.168.1.4>
2013-10-17 15:13:29 +0200

    change language support

--- SaleManager/src/main/java/com/hatde/salemanager/web/BundleBean.java
+++ SaleManager/src/main/java/com/hatde/salemanager/web/BundleBean.java
@@ -1,6 +1,7 @@
 package com.hatde.salemanager.web;
 
 import java.io.Serializable;
+import java.util.Locale;
 import java.util.ResourceBundle;
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.SessionScoped;
@@ -15,8 +16,8 @@ import org.primefaces.context.RequestContext;
 @Named(value = "bundleBean")
 @SessionScoped
 public class BundleBean implements Serializable {
-
     private ResourceBundle bundle;
+    private String updateString=":menuForm, :saleListForm, :buyListForm, :receiptListForm, :productListForm, :paymentListForm, :mainForm, :createSaleForm, :createBuyForm, :createReceiptForm, :createProductForm, :createPaymentForm, :createContactForm, :ContactListForm, :businessListForm, :businessContactForm";
 
     @PostConstruct
     public void init() {
@@ -31,7 +32,26 @@ public class BundleBean implements Serializable {
         this.bundle = bundle;
     }
 
+    public String getUpdateString() {
+        return updateString;
+    }
+
+    public void setUpdateString(String updateString) {
+        this.updateString = updateString;
+    }    
+    
     public void updateClient(final String updateString) {
         RequestContext.getCurrentInstance().update(updateString);
     }
+
+    public void changeLanguageVI() {
+        FacesContext.getCurrentInstance().getViewRoot().setLocale(new Locale("vi"));
+        init();
+    }
+
+    public void changeLanguageEN() {
+        FacesContext.getCurrentInstance().getViewRoot().setLocale(new Locale("en"));
+        init();
+    }
+
 }
