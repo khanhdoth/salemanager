f57b06ac8f28c6989598d7a9c9077858a8c6fb12
Do <Do@192.168.1.4>
2013-10-18 14:55:09 +0200

    test CI

--- SaleManager/src/test/java/com/hatde/salemanager/services/ContactFacadeRESTTest.java
+++ SaleManager/src/test/java/com/hatde/salemanager/services/ContactFacadeRESTTest.java
@@ -5,17 +5,18 @@
  */
 package com.hatde.salemanager.services;
 
-import com.hatde.salemager.EContainer;
 import com.hatde.salemanager.entities.Contact;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import javax.ejb.embeddable.EJBContainer;
+import javax.naming.NamingException;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
-import org.junit.Ignore;
 
 /**
  *
@@ -23,31 +24,38 @@ import org.junit.Ignore;
  */
 public class ContactFacadeRESTTest {
 
-    private static EJBContainer container;
     private Contact entity;
+    private static EJBContainer container;
+    private ContactFacadeREST instance;
 
     public ContactFacadeRESTTest() {
     }
 
     @BeforeClass
     public static void setUpClass() {
-        container = EContainer.INSTANCE.getContainer();
+        Map<Object, Object> properties = new HashMap<Object, Object>();
+        properties.put(EJBContainer.APP_NAME, "SaleManager");
+        container = javax.ejb.embeddable.EJBContainer.createEJBContainer(properties);
     }
 
     @AfterClass
     public static void tearDownClass() {
+        container.close();
     }
 
     @Before
-    public void setUp() {
-        entity = new Contact();        
+    public void setUp() throws NamingException {
+        instance = (ContactFacadeREST) container.getContext().lookup("java:global/SaleManager/classes/ContactFacadeREST");
+        System.out.print("instace == null? " + instance.toString());
+
+        entity = new Contact();
         entity.setAddress("Blumenfelstr. 7");
         entity.setCity("Essen");
         entity.setCountry("Germany");
         entity.setDistrict("Nordvietel");
         entity.setMoreInfo("Engineer");
         entity.setName("Khanh Do");
-        entity.setPostcode("45171");
+        entity.setPostcode("45171"); 
     }
 
     @After
@@ -55,194 +63,22 @@ public class ContactFacadeRESTTest {
     }
 
     /**
-     * Test of remove method, of class ContactFacadeREST.
-     */
-    @Ignore
-    @Test
-    public void testRemove_GenericType() throws Exception {
-        System.out.println("remove");
-        Contact entity = null;
-        
-        ContactFacadeREST instance = (ContactFacadeREST) container.getContext().lookup("java:global/classes/ContactFacadeREST");
-        instance.remove(entity);
-        
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of find method, of class ContactFacadeREST.
-     */
-    @Ignore
-    @Test
-    public void testFind_Object() throws Exception {
-        System.out.println("find");
-        Object id = null;
-        
-        ContactFacadeREST instance = (ContactFacadeREST) container.getContext().lookup("java:global/classes/ContactFacadeREST");
-        Contact expResult = null;
-        Contact result = instance.find(id);
-        assertEquals(expResult, result);
-        
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of findRange method, of class ContactFacadeREST.
-     */
-    @Ignore
-    @Test
-    public void testFindRange_intArr() throws Exception {
-        System.out.println("findRange");
-        int[] range = null;
-        
-        ContactFacadeREST instance = (ContactFacadeREST) container.getContext().lookup("java:global/classes/ContactFacadeREST");
-        List<Contact> expResult = null;
-        List<Contact> result = instance.findRange(range);
-        assertEquals(expResult, result);
-        
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of count method, of class ContactFacadeREST.
-     * @throws java.lang.Exception
-     */
-    @Ignore
-    @Test
-    public void testCount() throws Exception {
-        System.out.println("count");
-        
-        ContactFacadeREST instance = (ContactFacadeREST) container.getContext().lookup("java:global/classes/ContactFacadeREST");
-        int expResult = 0;
-        int result = instance.count();
-        assertEquals(expResult, result);
-        
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
      * Test of create method, of class ContactFacadeREST.
+     *
      * @throws java.lang.Exception
      */
+    //@Ignore
     @Test
     public void testCreate() throws Exception {
-        System.out.println("create");
-        
-        ContactFacadeREST instance = (ContactFacadeREST) container.getContext().lookup("java:global/classes/ContactFacadeREST");
+        System.out.println("testCreate");
+        //Contact entity2 = instance.find(entity.getContactId());
         instance.create(entity);
-        
-        Contact entity2 = instance.find(entity.getContactId());
-        
+        Contact entity2 = instance.find(entity.getContactId());        
         assertTrue(entity.equals(entity2));
+        
+        List listContact = instance.findAll();                
+        assertTrue(listContact.size()>0);
+        System.out.println("list size: " + listContact.size());
+        
     }
-
-    
-    /**
-     * Test of edit method, of class ContactFacadeREST.
-     */
-    @Ignore
-    @Test
-    public void testEdit() throws Exception {
-        System.out.println("edit");
-        Contact entity = null;
-        
-        ContactFacadeREST instance = (ContactFacadeREST) container.getContext().lookup("java:global/classes/ContactFacadeREST");
-        instance.edit(entity);
-        
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of remove method, of class ContactFacadeREST.
-     */
-    @Ignore
-    @Test
-    public void testRemove_Integer() throws Exception {
-        System.out.println("remove");
-        Integer id = null;
-        
-        ContactFacadeREST instance = (ContactFacadeREST) container.getContext().lookup("java:global/classes/ContactFacadeREST");
-        instance.remove(id);
-        
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of find method, of class ContactFacadeREST.
-     */
-    @Ignore
-    @Test
-    public void testFind_Integer() throws Exception {
-        System.out.println("find");
-        Integer id = null;
-        
-        ContactFacadeREST instance = (ContactFacadeREST) container.getContext().lookup("java:global/classes/ContactFacadeREST");
-        Contact expResult = null;
-        Contact result = instance.find(id);
-        assertEquals(expResult, result);
-        
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of findAll method, of class ContactFacadeREST.
-     */
-    @Ignore
-    @Test
-    public void testFindAll() throws Exception {
-        System.out.println("findAll");
-        
-        ContactFacadeREST instance = (ContactFacadeREST) container.getContext().lookup("java:global/classes/ContactFacadeREST");
-        List<Contact> expResult = null;
-        List<Contact> result = instance.findAll();
-        assertEquals(expResult, result);
-        
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of findRange method, of class ContactFacadeREST.
-     */
-    @Ignore
-    @Test
-    public void testFindRange_Integer_Integer() throws Exception {
-        System.out.println("findRange");
-        Integer from = null;
-        Integer to = null;
-        
-        ContactFacadeREST instance = (ContactFacadeREST) container.getContext().lookup("java:global/classes/ContactFacadeREST");
-        List<Contact> expResult = null;
-        List<Contact> result = instance.findRange(from, to);
-        assertEquals(expResult, result);
-        
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of countREST method, of class ContactFacadeREST.
-     * @throws java.lang.Exception
-     */
-    @Ignore
-    @Test
-    public void testCountREST() throws Exception {
-        System.out.println("countREST");
-        
-        ContactFacadeREST instance = (ContactFacadeREST) container.getContext().lookup("java:global/classes/ContactFacadeREST");
-        String expResult = "";
-        String result = instance.countREST();
-        assertEquals(expResult, result);
-        
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
 }
