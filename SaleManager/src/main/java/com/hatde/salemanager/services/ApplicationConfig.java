1336b87e476addfdf6eae135691e122961f16019
Do <Do@192.168.1.4>
2013-10-20 02:53:35 +0200

    report

--- SaleManager/src/main/java/com/hatde/salemanager/services/ApplicationConfig.java
+++ SaleManager/src/main/java/com/hatde/salemanager/services/ApplicationConfig.java
@@ -39,13 +39,6 @@ public class ApplicationConfig extends Application {
      * If required, comment out calling this method in getClasses().
      */
     private void addRestResourceClasses(Set<Class<?>> resources) {
-        resources.add(com.hatde.salemanager.services.BuyFacadeREST.class);
-        resources.add(com.hatde.salemanager.services.ContactFacadeREST.class);
-        resources.add(com.hatde.salemanager.services.PaymentReceivedFacadeREST.class);
-        resources.add(com.hatde.salemanager.services.PaymentSentFacadeREST.class);
-        resources.add(com.hatde.salemanager.services.PaymentTransactionFacadeREST.class);
-        resources.add(com.hatde.salemanager.services.ProductFacadeREST.class);
-        resources.add(com.hatde.salemanager.services.SaleFacadeREST.class);
         resources.add(org.eclipse.persistence.jpa.rs.exceptions.ClassNotFoundExceptionMapper.class);
         resources.add(org.eclipse.persistence.jpa.rs.exceptions.ConversionExceptionMapper.class);
         resources.add(org.eclipse.persistence.jpa.rs.exceptions.DatabaseExceptionMapper.class);
--- SaleManager/src/main/java/com/hatde/salemanager/web/ReportBean.java
+++ SaleManager/src/main/java/com/hatde/salemanager/web/ReportBean.java
@@ -2,6 +2,7 @@ package com.hatde.salemanager.web;
 
 import com.hatde.salemanager.entities.PaymentTransaction;
 import com.hatde.salemanager.entities.Sale;
+import com.hatde.salemanager.entities.SaleItem;
 import java.io.File;
 import java.io.InputStream;
 import java.io.FileInputStream;
@@ -22,7 +23,12 @@ import org.docx4j.openpackaging.exceptions.Docx4JException;
 import org.docx4j.openpackaging.exceptions.InvalidFormatException;
 import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
 import org.docx4j.openpackaging.parts.WordprocessingML.MainDocumentPart;
+import org.docx4j.wml.P;
+import org.docx4j.wml.R;
+import org.docx4j.wml.Tbl;
+import org.docx4j.wml.Tc;
 import org.docx4j.wml.Text;
+import org.docx4j.wml.Tr;
 import org.primefaces.model.DefaultStreamedContent;
 import org.primefaces.model.StreamedContent;
 
@@ -51,14 +57,7 @@ public class ReportBean implements Serializable {
         }
     }
     
-    public ReportBean() {
-        /*
-        try {
-            WordprocessingMLPackage wordMLPackage = WordprocessingMLPackage.load(new File(bundleBean.getBundle().getString("PathSalesOrderTemplate")));
-        } catch (Docx4JException ex) {
-            Logger.getLogger(ReportBean.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        */
+    public ReportBean() {       
     }
 
     public void createStockOutForm(Sale sale) {
@@ -68,53 +67,71 @@ public class ReportBean implements Serializable {
             System.out.println("load ok!");
             MainDocumentPart mdp = wordMLPackage.getMainDocumentPart();
 
-            String xpath = "//w:t[contains(text(),'[')]";
+            String xpath = "//w:t[contains(text(),'=')]";
             List<Object> list = mdp.getJAXBNodesViaXPath(xpath, false);
             for (Object e : list) {
                 Text t = (Text) ((JAXBElement) e).getValue();
 
                 String replacedText = "";
                 switch (t.getValue()) {
-                    case "[NO.]":
+                    case "=SID":
                         replacedText = sale.getProductTransactionId() + "";
                         break;
 
-                    case "[CLICK TO SELECT DATE]":
+                    case "=DATE":
                         replacedText = customFormatDate(sale.getDate());
                         break;
 
-                    case "[Contact Name]":
+                    case "=Contact Name":
                         replacedText = sale.getContact().getName();
                         break;
 
-                    case "[Subtotal]":
+                    case "=Subtotal":
                         replacedText = customFormatNumber(amountFormat, sale.getSubTotal());
                         break;
 
-                    case "[Tax]":
+                    case "=Tax":
                         replacedText = customFormatNumber(amountFormat, sale.getAmountVAT());
                         break;
 
-                    case "[Total]":
+                    case "=Total":
                         replacedText = customFormatNumber(amountFormat, sale.getAmount());
                         break;
 
-                    case "[Paid]":
+                    case "=Paid":
                         if (sale.getPayment() instanceof PaymentTransaction) {
                             replacedText = "-" + customFormatNumber(amountFormat, sale.getPayment().getAmount());
                         }
                         break;
 
-                    case "[BalanceDue]":
+                    case "=BalanceDue":
                         replacedText = customFormatNumber(amountFormat, sale.getAmountAfterPayment());
                         break;
 
                     default:
-                        replacedText = "#####";
+                        replacedText = "----";
                 }
 
                 t.setValue(replacedText);
             }
+            
+            //Get table
+            xpath = "//w:tbl";
+            list = mdp.getJAXBNodesViaXPath(xpath, false);
+            Tbl dataTable = (Tbl) ((JAXBElement) list.get(3)).getValue();
+            
+            List rows = dataTable.getContent();
+            int rowIndex = 1;
+            for(SaleItem si: sale.getListOfSaleItem()){
+                Tr cRow = (Tr) rows.get(rowIndex);                
+                List cols = cRow.getContent();
+                Tc col0 = (Tc) ((JAXBElement)cols.get(0)).getValue();
+                Text col0Text = (Text)((JAXBElement) ((R)(((P) col0.getContent().get(0)).getContent().get(0))).getContent().get(0)).getValue();
+                col0Text.setValue(si.getQuantity()+"");
+                rowIndex++;
+            }
+            
+            
             String outFile = bundleBean.getBundle().getString("PathSalesOrderResult") + ".docx";
             wordMLPackage.save(new java.io.File(outFile));
 
